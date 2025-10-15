aac57853250860e0c770d8b42d283d67c7d691a4
Padala Raghavendra Reddy <raghavareddy696969@gmail.com>
2025-10-15 09:53:41 +0530

    final addons

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCGBDh2B1DE-ENrUEjJQCRWlHf0PPVLenY",
-  authDomain: "huskthreadss.firebaseapp.com",
-  projectId: "huskthreadss",
-  storageBucket: "huskthreadss.firebasestorage.app",
-  messagingSenderId: "226432782763",
-  appId: "1:226432782763:web:41a3612db6f857e5a5c2ee"
+  apiKey: "AIzaSyCNjav-LyjYVadHOpJd72E3sxn0VVRKsrk",
+  authDomain: "huskthreads1.firebaseapp.com",
+  projectId: "huskthreads1",
+  storageBucket: "huskthreads1.firebasestorage.app",
+  messagingSenderId: "362237659247",
+  appId: "1:362237659247:web:9937d673bf7f79974ca59a"
 };
 
 const app = initializeApp(firebaseConfig);
